0b5c0d61618635a9b8840e03314c38782d95be27
faspen <2335@holbertonschool.com>
2021-06-01 18:51:40 +0000

    piecing together

--- dragonfly/__main__.py
+++ dragonfly/__main__.py
@@ -1,16 +1,21 @@
 """Main file for dragonfly function"""
-import sys
+from sys import argv
+from .analysis import analysis
+from .to_json_string import to_json
 
 
-opts = [opt for opt in sys.argv[1:] if opt.startswith("--")]
+opts = [opt for opt in argv[1:] if opt.startswith("--")]
 
 
 def main():
     """Main func"""
+    data = {}
     if "--version" in opts:
         print("0.1.0")
+        return
     if "--noises" in opts:
         print("bzzz bzzz bzzz")
+        return
     if "--help" in opts:
         print(
             "usage: dragonfly [folder/file]...\n\n"
@@ -18,6 +23,14 @@ def main():
             "Options:\n"
             "\t--version\tprint version\n"
             "\t--help\t   print this message\n")
+        return
+
+    if len(argv) is 1:
+        data = analysis(argv[1])
+        to_json(data, drgnfly_analysis)
+    if len(argv) is 2:
+        data = analysis(argv[1], argv[2])
+        to_json(data, drgnfly_analysis)
 
 if __name__ == "__main__":
     main()
