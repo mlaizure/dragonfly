ece6781912bfb9c502dfab311a929f137bf4a9bd
mlaizure <maddi.laizure@gmail.com>
2021-06-02 05:02:12 +0000

    Fixing imports

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 """Main file for dragonfly function"""
 from sys import argv
-from .analysis import analysis
-from .to_json_string import to_json
+from analysis.analysis import analysis
+from analysis.to_json_string import to_json
 
 
 opts = [opt for opt in argv[1:] if opt.startswith("--")]
@@ -18,19 +19,19 @@ def main():
         return
     if "--help" in opts:
         print(
-            "usage: dragonfly [folder/file]...\n\n"
-            "  Return commit data from given folder or file.\n\n"
+            "usage: dragonfly ABSOLUTE_PATH_TO_REPO [BRANCH]\n\n"
+            "  Return commit data from given repository.\n\n"
             "Options:\n"
             "\t--version\tprint version\n"
             "\t--help\t   print this message\n")
         return
 
-    if len(argv) is 1:
+    if len(argv) == 2:
         data = analysis(argv[1])
-        to_json(data, drgnfly_analysis)
-    if len(argv) is 2:
+        to_json(data, 'drgnfly_analysis')
+    if len(argv) == 3:
         data = analysis(argv[1], argv[2])
-        to_json(data, drgnfly_analysis)
+        to_json(data, 'drgnfly_analysis')
 
 if __name__ == "__main__":
     main()
--- analysis/to_json_string.py
+++ analysis/to_json_string.py
@@ -5,6 +5,5 @@
 def to_json(adict, filename):
     """accept dict, convert to json string, save to file"""
     import json
-    with open("filename" + ".json", "w") as outfile:
+    with open(filename + ".json", "w") as outfile:
         json.dump(adict, outfile)
-
