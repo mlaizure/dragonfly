bb17a636995493a372ca0737b148a3b28531956e
faspen <2335@holbertonschool.com>
2021-06-03 00:11:11 +0000

    added a crude loading bar

--- dragonfly/analysis.py
+++ dragonfly/analysis.py
@@ -7,7 +7,14 @@ def analysis(repo, branch='master'):
 
     repo.git.checkout(branch)
 
+    toolbar_width = 0
+
+    sys.stdout.write("[%s]" % (" " * toolbar_width))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (toolbar_width + 1))
+
     for i, commit in enumerate(repo.iter_commits(branch)):
+        time.sleep(0.1)
         keywords = ['fix', 'bug', 'issue']
         if any(keyword in commit.message for keyword in keywords):
             change_types = ['A', 'C', 'D', 'R', 'M', 'T']
@@ -19,8 +26,11 @@ def analysis(repo, branch='master'):
                             num_fixes[diff.a_path] += 1
                         else:
                             num_fixes[diff.a_path] = 1
+        sys.stdout.write("-")
+        sys.stdout.flush()
 
 #    print(num_fixes)
+    sys.stdout.write("] Analysis Complete!\n")
     return num_fixes
 
 if __name__ == '__main__':
