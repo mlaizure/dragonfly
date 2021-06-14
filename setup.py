a36e0af9c8ed5021aa76862b5ad832a1d33e85ca
mlaizure <maddi.laizure@gmail.com>
2021-06-14 04:10:21 +0000

    Adding package info

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 import setuptools
 
 setuptools.setup(
-    name = 'git-dragonfly',
-    version = '0.1.5',
-    packages = setuptools.find_packages(),
-    entry_points = {
+    name='git-dragonfly',
+    version='0.1.6',
+    description='Bug analysis on Git repositories',
+    author='Finn Aspenson, Corbin Vandeventer, Maddi Laizure',
+    url='https://mlaizure.github.io/dragonfly/',
+    packages=setuptools.find_packages(),
+    entry_points={
         'console_scripts': [
             'dragonfly = dragonfly.__main__:main'
         ]
     },
-    install_requires = [
+    install_requires=[
         'gitpython',
         'matplotlib'
     ]
